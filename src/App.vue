e0a7d4b4968254c716480dc9b1c250c717b7ee8d
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-05-29 17:26:25 -0300

    profile file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-		<h1>Rotas com VueRouter</h1> <a @click="logout" v-if="usuario">{{ usuario }} | Sair</a>
+		<h1>Rotas com VueRouter</h1> <a @click="logout" v-if="usuario" v-html="usuario"></a>
 		<router-view name="menu"></router-view>
 		<transition mode="out-in"
 			enter-active-class="animated rubberBand"
@@ -29,7 +29,9 @@ export default {
 	},*/
 	 watch: {
 		'$route' (to, from) {
-			this.usuario = localStorage.getItem('credenciais') ? `Olá ${JSON.parse(localStorage.getItem('credenciais')).user.name}` : ''
+			var url_storage = process.env.VUE_APP_ENV_URL_S3
+			if( /local/.test(process.env.VUE_APP_ENV_API_URL) ) url_storage = process.env.VUE_APP_ENV_API_URL
+			this.usuario = localStorage.getItem('credenciais') ? `Olá ${JSON.parse(localStorage.getItem('credenciais')).user.name} | Sair <br> <img src='${url_storage}/${JSON.parse(localStorage.getItem('credenciais')).user.profile}' />` : ''
 		}
   	}
 
